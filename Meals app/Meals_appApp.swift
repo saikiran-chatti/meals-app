4b8b55b325eb2dbd7776c74bc28ee43ab7b1c646
saikiran-chatti <jayanthsaikiran@gmail.com>
2024-05-23 21:28:41 -0400

    Initial Commit

---
+++ Meals app/Meals_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meals_appApp.swift
+//  Meals app
+//
+//  Created by Kiran Chatti on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Meals_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
