64ab6d8cf684cb32f7e7d72d6aea83e1b3e0158f
saikiran-chatti <jayanthsaikiran@gmail.com>
2024-05-23 22:12:25 -0400

    Code setup

---
+++ Meals app/Models/MealModels.swift
@@ -0,0 +1,8 @@
+//
+//  MealModels.swift
+//  Meals app
+//
+//  Created by Kiran Chatti on 5/23/24.
+//
+
+import Foundation
---
+++ Meals app/Network/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Meals app
+//
+//  Created by Kiran Chatti on 5/23/24.
+//
+
+import Foundation
